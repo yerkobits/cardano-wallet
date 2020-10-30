c2f333e20baa264f2f8384c587f96aa47d409531
Johannes Lund <johannes.lund@iohk.io>
2020-10-30 10:50:01 +0100

    Rebase fixups

--- lib/core-integration/src/Test/Integration/Scenario/API/Byron/Wallets.hs
+++ lib/core-integration/src/Test/Integration/Scenario/API/Byron/Wallets.hs
@@ -39,7 +39,7 @@ import Control.Monad.IO.Class
 import Control.Monad.Trans.Resource
     ( runResourceT )
 import Data.Generics.Internal.VL.Lens
-    ( (^.) )
+    ( view, (^.) )
 import Data.Maybe
     ( isJust, isNothing )
 import Data.Proxy
