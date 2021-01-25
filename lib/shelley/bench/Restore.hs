2f8a88d930e0f5cd38635d0d37aa4e5698d069fd
Johannes Lund <johannes.lund@iohk.io>
2021-01-25 15:35:19 +0100

    Try lowering number wallets in bench

--- lib/shelley/bench/Restore.hs
+++ lib/shelley/bench/Restore.hs
@@ -292,9 +292,7 @@ cardanoRestoreBench tr c socketFile = do
           benchRestoreMultipleWallets 1 (unsafeMkPercentage 0.1)
         , benchRestoreMultipleWallets 10 (unsafeMkPercentage 0.01)
         , benchRestoreMultipleWallets 20 (unsafeMkPercentage 0.01)
-        , benchRestoreMultipleWallets 40 (unsafeMkPercentage 0.01)
-        , benchRestoreMultipleWallets 80 (unsafeMkPercentage 0.01)
-        , benchRestoreMultipleWallets 100 (unsafeMkPercentage 0.01)
+        , benchRestoreMultipleWallets 30 (unsafeMkPercentage 0.01)
 
         , benchRestoreSeqWithOwnership (Proxy @0)
         , benchRestoreSeqWithOwnership (Proxy @1)
